40c09aa9942f227407ddd63ffd65cdae9154dc29
grantgabriel <grantgabriel30@gmail.com>
2023-06-24 08:13:26 +0700

    Add some solutions

--- Gemastik/2021/Penyisihan/Jawaban/[J] Maximum Sum of Array.cpp
+++ Gemastik/2021/Penyisihan/Jawaban/[J] Maximum Sum of Array.cpp
@@ -16,10 +16,13 @@ int main() {
     for(int i = 0; i < N; i++) {
         if(i >= M) {
             maksimum = max(maksimum, array[i - M]);
+            cout << "Maksimum " << i << " : " << maksimum << endl;
             array[i] += maksimum;
+            cout << "Array i setelah dijumlah dengan maksimum i " << array[i] << endl;
         }
 
         jumlah = max(array[i], jumlah);
+        cout << "Jumlah : " << jumlah << "\n" << endl;
     }
 
     cout << jumlah;
@@ -27,31 +30,5 @@ int main() {
     return 0;
 }
 
-/*
-5 4 
-2 7 -5 1 4
-
-Iterasi pertama : i = 0
-i >= M // Salah
-jumlah = 2; // 2 banding 0
-
-Iterasi kedua : i = 1
-i >= M // Salah
-jumlah = 7; // 7 banding 2
-
-Iterasi ketiga : i = 2
-i >= M // Salah 
-jumlah = 7; // 7 banding -5
-
-Iterasi keempat : i = 3
-i >= M // Salah
-jumlah = 7; // 7 banding -1
-
-Iterasi kelima : i = 4
-i >= M // Benar
-maksimum = 11
-
-
-
-
-*/
+// 10 2
+// -1 2 5 7 -1 2 3 -2 4 8
--- Gemastik/2021/Penyisihan/Jawaban/testing.cpp
+++ Gemastik/2021/Penyisihan/Jawaban/testing.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-// Fungsi untuk menghitung faktorial dengan modulo
-int factorial(int n) {
-    const int MOD = 1000000007;
-    int result = 1;
-    for (int i = 1; i <= n; i++) {
-        result = (result * i) % MOD;
+vector<int> find_longest_prefix(const string& s) {
+    int n = s.length();
+    vector<int> lps(n, 0);
+    int length = 0;
+    int i = 1;
+
+    while (i < n) {
+        if (s[i] == s[length]) {
+            length++;
+            lps[i] = length;
+            i++;
+        } else {
+            if (length != 0) {
+                length = lps[length - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
     }
-    return result;
+
+    return lps;
 }
 
 int main() {
-    int N, a, b, c, d;
-    std::cin >> N >> a >> b >> c >> d;
+    string S;
+    cin >> S;
+    int Q;
+    cin >> Q;
 
-    // Hitung jumlah kemungkinan urutan soal
-    int total = factorial(a-1) * factorial(b-a-1) * factorial(c-b-1) * factorial(N-d);
-    total %= 1000000007;
+    for (int q = 0; q < Q; q++) {
+        int operation, a, b;
+        cin >> operation >> a >> b;
 
-    std::cout << total << std::endl;
+        if (operation == 1) {
+            char x;
+            cin >> x;
+            S[a] = x;
+        } else if (operation == 0) {
+            string substring = S.substr(a, b - a + 1);
+            string prefix = substring;
+            reverse(substring.begin(), substring.end());
+            vector<int> lps = find_longest_prefix(substring);
+            int longest_prefix_length = lps.back();
+
+            cout << longest_prefix_length << endl;
+        }
+    }
 
     return 0;
 }
