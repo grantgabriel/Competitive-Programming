9fac1a2274538d5a190ab7f2e394acaced80e616
grantgabriel <grantgabriel30@gmail.com>
2023-09-09 17:09:40 +0700

    Finally finish prob A

--- TLX Toki/Pemrograman Kompetitif Dasar/1. Perkenalan Pemrograman Kompetitif/Submission/A.cpp
+++ TLX Toki/Pemrograman Kompetitif Dasar/1. Perkenalan Pemrograman Kompetitif/Submission/A.cpp
@@ -1,124 +1,124 @@
+#include <map>
 #include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 #define boost ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define debug(a) cout << a << endl
+vector<string> ans;
 
-struct pemain {
-    string ID;
-    int N1, N2, N3;
-};
+bool desc(int a, int b) {
+    return a > b;
+}
 
 void run_cases() {
-    int N, M;
-    cin >> N >> M;
-    string TID; cin >> TID;
-    int T1, T2, T3;
+    int N, M; cin >> N >> M; string target; cin >> target;
+    vector<int> grade1, grade2, grade3;
+    int TA, TB, TC;
 
-    vector<pemain> peserta(N);
-    for(auto &r : peserta) {
-        cin >> r.ID >> r.N1 >> r.N2 >> r.N3;
-        if(r.ID == TID) {
-            T1 = r.N1;
-            T2 = r.N2;
-            T3 = r.N3;
+    for(int i = 0; i < N ; i++) {
+        string temp; cin >> temp;
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(temp == target) {
+            TA = a; TB = b; TC = c;
         }
+        grade1.emplace_back(a);
+        grade2.emplace_back(b);
+        grade3.emplace_back(c);
     }
+
+    map<int, int> map3;
+    for(int i = 0 ; i < N; i++) {
+        map3[grade3[i]]++;
+    }
+
+    sort(grade3.begin(), grade3.end(), desc);
+    grade3.erase(unique(grade3.begin(), grade3.end()), grade3.end());
+    
+    bool kena = false;
+    for(int i = 0; i < grade3.size(); i++) {
+        // cout << "Testing grade3 #" << i << " " << grade3[i] << ' ' << map3[grade3[i]] << endl; 
+        if(grade3[i] == TC) kena = true;
+
+
+        if(grade3[i] == TC and M >= map3[grade3[i]]) {
+            ans.push_back("YA"); return;
+        } else if(M <= 0) {
+            ans.push_back("TIDAK"); return;
+        } else if(!kena){
+            M -= map3[grade3[i]];
+            // cout << "M grade3 #" << i << " " << M << endl;
+        }
+
+        if(kena) break;
+    }    
+
+    kena = false;
+    map<int, int> map2;
+    for(int i = 0 ; i < N; i++) {
+        map2[grade2[i]]++;
+    }
+
+    sort(grade2.begin(), grade2.end(), desc);
+    grade2.erase(unique(grade2.begin(), grade2.end()), grade2.end());
+
+    for(int i = 0; i < grade2.size(); i++) {
+        // cout << "Testing grade2 #" << i << " " << grade2[i] << ' ' << map2[grade2[i]] << endl; 
+        if(grade2[i] == TB) kena = true;
+
+        if(grade2[i] == TB and M >= map2[grade2[i]]) {
+            ans.push_back("YA"); return;
+        } else if(M <= 0) {
+            ans.push_back("TIDAK"); return;
+        } else if(!kena) {
+            M -= map2[grade2[i]];
+            // cout << "M grade2 #" << i << " " << M << endl;
+        }
+
+        if(kena) break;
+    }
+
+    kena = false;
+    map<int, int> map1;
+    for(int i = 0 ; i < N; i++) {
+        map1[grade1[i]]++;
+    } 
+
+    sort(grade1.begin(), grade1.end(), desc);
+    grade1.erase(unique(grade1.begin(), grade1.end()), grade1.end());
+    
+    for(int i = 0; i < grade1.size(); i++) {
+        // cout << "Testing grade1 #" << i << " " << grade1[i] << ' ' << map1[grade1[i]] << endl; 
+        if(grade1[i] == TA) kena = true;
+
+        if(grade1[i] == TA and M >= map1[grade1[i]]) {
+            ans.push_back("YA"); return;
+        } else if(M <= 0) {
+            ans.push_back("TIDAK"); return;
+        } else {
+            M -= map1[grade1[i]];
+            // cout << "M grade2 #" << i << " " << M << endl;
+        }
+
+        if(kena) ans.push_back("TIDAK");
+    }
+
 }
 
 int main() {
     boost;
     
-    int T; cin >> T;
+    int T, i = 1; cin >> T;
     while(T--) {
-        run_cases();
-        cout << flush;
+        // cout << "CASES #" << i << endl;
+        run_cases(); i++;
+    }
+
+    for(const auto &r : ans) {
+        cout << r << endl;
     }
     
     return 0;
 }
-
-// int find_index(vector<int> &a, int key) {
-//     int i = 1;
-//     for(auto &num : a) {
-//         if(num == key) return i;
-
-//         i++;
-//     }
-
-//     return -1;
-// }
-
-// int count_element(vector<int> &a, int target) {
-//     int i = 0;
-
-//     for(auto &num : a) {
-//         if(num == target) i++;
-//     }
-
-//     return i;
-// }
-
-// int count_before_element(vector<int> &a, int target) {
-//     int i = 0;
-    
-//     for(auto &num : a) {
-//         if(num == target) {
-//             return i;
-//         } else {
-//             i++;
-//         }
-//     }
-
-//     return -1;
-// }
-
-// void run_cases() {
-//     int N, M; cin >> N >> M; string target; cin >> target;
-//     vector<int> grade1, grade2, grade3;
-//     int TA, TB, TC;
-
-//     for(int i = 0; i < N ; i++) {
-//         string temp; cin >> temp;
-//         int a, b, c;
-//         cin >> a >> b >> c;
-//         if(temp == target) {
-//             TA = a; TB = b; TC = c;
-//         }
-//         grade1.emplace_back(a);
-//         grade2.emplace_back(b);
-//         grade3.emplace_back(c);
-//     }
-
-//     sort(grade3.begin(), grade3.end());
-//     int curr = find_index(grade3, TC);
-//     if(curr <= M and count_element(grade3, TC) < 2) { 
-//         cout << "YA" << endl; return;
-//     } else if(grade3[M - 1] > TC){
-//         cout << "TIDAK" << endl; return;
-//     } else {
-//         int before = count_before_element(grade3, TC);
-//         M = M - before;
-//     }
-
-//     sort(grade2.begin(), grade2.end());
-//     curr = find_index(grade2, TB);
-//     if(curr <= M and count_element(grade2, TB) < 2) { 
-//         cout << "YA" << endl; return;
-//     } else if(grade2[M - 1] > TB){
-//         cout << "TIDAK" << endl; return;
-//     } else {
-//         int before = count_before_element(grade2, TB);
-//         M = M - before;
-//     }
-
-//     sort(grade1.begin(), grade1.end());
-//     curr = find_index(grade1, TA);
-//     if(curr <= M) {
-//         cout << "YA"; return;
-//     } else {
-//         cout << "TIDAK"; return;
-//     }
-    
-// }
