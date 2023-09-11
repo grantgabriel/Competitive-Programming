55586efd50b2bbe0adbb57f3d60af24ff374274a
grantgabriel <grantgabriel30@gmail.com>
2023-09-11 19:49:47 +0700

    RTE on prob B

--- TLX Toki/Pemrograman Kompetitif Dasar/1. Perkenalan Pemrograman Kompetitif/Submission/B.cpp
+++ TLX Toki/Pemrograman Kompetitif Dasar/1. Perkenalan Pemrograman Kompetitif/Submission/B.cpp
@@ -1,46 +1,85 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define bug(a) cout << a
 #define bugs(a) cout << a << ' '
 #define debug(a) cout << a << endl
 
+int satuBaris[8], akhirBaris[8];
+int R, C, lastRow; 
+vector<string> row;
+string full = "", emp = "";
+
+bool adaLayakRuntuh() {
+    for(auto &r : row)
+        if(r == full) return true;
+    return false;
+}
+
+void hitungAtas() {
+    for(int i = 0; i < C; i++) {
+        for(int j = 0; j < lastRow; j++) {
+            if(row[j][i] == '1') {
+                satuBaris[i] += 1;
+                row[j][i] = '0';
+            }
+        }
+    }
+}
+
+void hitungBawah() {
+    for(int i = 0; i < C; i++) {
+        for(int j = R - 1; j > lastRow; j--) {
+            if(row[j][i] == '1') {
+                akhirBaris[i] = j; 
+            }
+        }
+    }
+}
+
+void stackBawah() {
+    for(int i = 0; i < C; i++) {
+        for(int j = akhirBaris[i] - 1; satuBaris[i] > 0; j--) {
+            row[j][i] = '1';
+            satuBaris[i]--;
+        }
+    }
+}
+
+void peruntuhan() {
+    lastRow = 0; int ite = 0;
+    for(auto &r : row) {
+        if(r == full) {
+            r = emp;
+            lastRow = ite;
+        } 
+        ite++;
+    }
+
+    hitungAtas();
+    hitungBawah();
+    stackBawah();
+}
+
 int main() {    
-    int R, C; cin >> R >> C;
-    string full = "", empty = "";
-    vector<string> row;
+    memset(satuBaris, 0, sizeof(satuBaris));
+    cin >> R >> C;
 
     for(int i = 0; i < C; i++) {
         full += "1";
-        empty += "0";
+        emp += "0";
     }
-    
+
     for(int i = 0; i < R; i++) {
         string temp; cin >> temp;
-        if(temp == full) {
-            row.push_back(empty);
-        } else {
-            row.push_back(temp);
-        }
+        row.push_back(temp);
     }
 
-    for(int i = 0; i < R - 1; i++) {
-        string A = row[i], B = row[i + 1]; 
-        for(int j = 0; i < C; j++) {
-            if(A[j] == '1' and B[j] == '0') {
-                A[j] = '0';
-                B[j] = '1';
-            }
-        }
-        row[i] = A; row[i + 1] = B;
-    }
+    while(adaLayakRuntuh())
+        peruntuhan();
 
-    for(int i = 0; i < R; i++) {
-        cout << row[i] << endl;
-    } 
+    for(auto &r : row)
+        cout << r << endl;
 
     return 0;
 }
