0ea39731a4e533b25c03e36cd773aac732a66140
grantgabriel <grantgabriel30@gmail.com>
2023-10-03 20:14:27 +0700

    Add unfinished B

--- Learning Progress/TLX Toki/Pemrograman Kompetitif Dasar/3. Pencarian dan Pengurutan/Submission/B.cpp
+++ Learning Progress/TLX Toki/Pemrograman Kompetitif Dasar/3. Pencarian dan Pengurutan/Submission/B.cpp
@@ -7,23 +7,81 @@ using namespace std;
 
 #define ll long long
 #define ull unsigned long long
-#define bug(a) cout << a
-#define bugs(a) cout << a << ' '
 #define debug(a) cout << a << endl
 #define boost ios_base::sync_with_stdio(false); cin.tie(NULL)
+vector<pair<int, int>> ans; bool ada = false;
+int N, M, K; int grid[100][100];
+
+void countNeighbors(int x, int y) {
+	ull total;
+	if(y == 0 and x == 0) {
+		total = grid[x + 1][y] * grid[x][y + 1];
+	} else if(x == 0 and y == M - 1) {
+		total = grid[x][y - 1] * grid[x + 1][y];
+	} else if(x == N - 1 and y == 0) {
+		total = grid[x - 1][y] * grid[x][y + 1];
+	} else if(x == N - 1 and y == M - 1) {
+		total = grid[x][y - 1] * grid[x - 1][y];
+	} else if(x == 0) {
+		total = grid[x][y - 1] * grid[x + 1][y] * grid[x][y + 1];
+	} else if(y == 0) {
+		total = grid[x + 1][y] * grid[x - 1][y] * grid[x][y + 1];
+	} else if(x == N - 1) {
+		total = grid[x][y - 1] * grid[x - 1][y] * grid[x][y + 1];
+	} else if(y == M - 1) {
+		total = grid[x + 1][y] * grid[x - 1][y] * grid[x][y - 1];
+	} else {
+		total = grid[x][y + 1] * grid[x][y - 1] * grid[x + 1][y] * grid[x - 1][y];
+	}
+
+	if(total == K) {
+		ans.push_back(make_pair(x, y)); ada = true;
+	} 
+}
+
+void findAnswer() {
+	// for(auto &r : ans) { 
+	// 	cout << r.first << " " << r.second << endl;
+	// }
+	
+	if(!ada) {
+		cout << "0 0"; return;
+	}
+	
+	int temp;
+	bool doubly = false;
+
+	sort(ans.begin(), ans.end());
+
+
+	if(ans[0].first == ans[1].first) {
+		temp = min(ans[0].second, ans[1].second);
+		cout << ans[0].first << " " << temp;
+	} else {
+		cout << ans[0].first << " " << ans[0].second;
+	}
+
+	
+
+}
 
 int main() {
 	boost;
+	cin >> N >> M >> K;
 	
-	int N, M, K; cin >> N >> M >> K;
-
-	int grid[N][M]; vector<pair<int, int>> ans;
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < N; i++) {
 		for(int j = 0; j < M; j++) {
-			int temp; cin >> temp;
-			grid[i][j] = temp;
-		}	
+			cin >> grid[i][j];
+		}		
+	}
 
+	for(int x = 0; x < N; x++) {
+		for(int y = 0; y < M; y++) {
+			countNeighbors(x, y);
+		}
+	}
+
+	findAnswer();
 	
 	return 0;
 }
