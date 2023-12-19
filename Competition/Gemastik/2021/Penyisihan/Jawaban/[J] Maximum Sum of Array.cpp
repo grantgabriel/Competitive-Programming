4b96fd74449f572e27bee44071a27e335fff1a0e
grantgabriel <grantgabriel30@gmail.com>
2023-12-20 02:06:07 +0700

    Testing VIM environment on this thing

--- Competition/Gemastik/2021/Penyisihan/Jawaban/[J] Maximum Sum of Array.cpp
+++ Competition/Gemastik/2021/Penyisihan/Jawaban/[J] Maximum Sum of Array.cpp
@@ -1,34 +1,21 @@
-#include <iostream>
-#include <math.h>
+#include <bits/stdc++.h>
 #define boost ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 
 int main() {
     boost;
 
-    int N, M, maksimum = 0, jumlah = 0;
+
+	int N, M, SUM;
     cin >> N >> M;
+	vector<int> nums(N);
+	for(auto &num : nums) {
+		cin >> num;
+	}
 
-    int array[N];   
-    for(int i = 0; i < N; i++)
-        cin >> array[i];
+	for(int num : nums) {
 
-    for(int i = 0; i < N; i++) {
-        if(i >= M) {
-            maksimum = max(maksimum, array[i - M]);
-            // cout << "Maksimum " << i << " : " << maksimum << endl;
-            array[i] += maksimum;
-            // cout << "Array i setelah dijumlah dengan maksimum i " << array[i] << endl;
-        }
-
-        jumlah = max(array[i], jumlah);
-        // cout << "Jumlah : " << jumlah << "\n" << endl;
-    }
-
-    cout << jumlah;
 
     return 0;
 }
 
-// 10 2
-// -1 2 5 7 -1 2 3 -2 4 8
